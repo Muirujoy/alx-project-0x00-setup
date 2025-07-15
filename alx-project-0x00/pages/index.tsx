168f60c6ff65fa718c2f113571c1ed4501be97a5
Joy <muirujoybackup@gmail.com>
2025-07-15 19:23:00 +0300

    added

--- alx-project-0x00/pages/index.tsx
+++ alx-project-0x00/pages/index.tsx
@@ -1,15 +1,10 @@
-import Link from 'next/link';
-
 const Home: React.FC = () => {
   return (
     <main className="flex flex-col h-screen justify-center items-center text-4xl font-semibold">
       <h1>Airbnb Application Clone system</h1>
-
-      <Link href="/landing">
-        <button className="border px-3 py-1 text-lg mt-3 bg-blue-500 text-white rounded-full">
-          Get Started
-        </button>
-      </Link>
+      <button className="border px-3 py-1 text-lg mt-3 bg-blue-500 text-white rounded-full">
+        Get Started
+      </button>
     </main>
   );
 };
--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -1,14 +1,11 @@
 import React from "react";
-import Card from "../components/Card";
-
+import Card from "../components/Card"; // Or use alias if properly configured
 
 const Landing: React.FC = () => {
   return (
-    <div className="p-6">
-      <h1 className="text-xl font-extralight mb-6">Landing Page</h1>
-
-      {/* Reusable Card components */}
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+    <div className="p-8">
+      <h1 className="text-xl font-extralight mb-4">Landing Page</h1>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         <Card />
         <Card />
         <Card />
