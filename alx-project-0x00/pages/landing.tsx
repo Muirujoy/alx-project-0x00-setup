17b9dc98f53034ffd2e8bfef8e7995b9e85b69b8
Joy <muirujoybackup@gmail.com>
2025-07-15 17:56:10 +0300

    added

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -1,8 +1,19 @@
-const Landing: React.FC =  () => {
+import React from "react";
+import Card from "@/components/Card";
+
+const Landing: React.FC = () => {
   return (
-    <div>
-      <h1 className=" text-xl font-extralight">Landing Page</h1>
+    <div className="p-6">
+      <h1 className="text-xl font-extralight mb-6">Landing Page</h1>
+
+      {/* Reusable Card components */}
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+        <Card />
+        <Card />
+        <Card />
+      </div>
     </div>
-  )
-}
-export default Landing
+  );
+};
+
+export default Landing;
